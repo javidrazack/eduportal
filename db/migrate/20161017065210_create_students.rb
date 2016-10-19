b42b1bb6713608adf289fa398ddd6e7a1d6e1160
Javid <javid.razak@gmail.com>
2016-10-20 00:03:22 +0530

    Added attributes via db migration files

--- db/migrate/20161017065210_create_students.rb
+++ db/migrate/20161017065210_create_students.rb
@@ -2,6 +2,26 @@ class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
 
+      t.string     :admission_no
+      t.string     :class_roll_no
+      t.date       :admission_date
+
+      t.string     :first_name
+      t.string     :middle_name
+      t.string     :last_name
+
+      t.references :batch
+      t.date       :date_of_birth
+      t.string     :gender
+      t.string     :blood_group
+      t.references :student_category
+
+      t.string     :address_line1
+      t.string     :address_line2
+      t.string     :city
+      t.string     :state
+      t.string     :pin_code
+      t.string     :phone1
       t.timestamps
     end
   end
--- db/migrate/20161017065509_create_attendances.rb
+++ db/migrate/20161017065509_create_attendances.rb
@@ -1,7 +1,10 @@
 class CreateAttendances < ActiveRecord::Migration[5.0]
   def change
     create_table :attendances do |t|
-
+      t.references :student
+      t.string :nhours
+      t.references :subID
+      t.boolean :present, :default => true
       t.timestamps
     end
   end
